d0e10003f33e07281644ef45168d74b409a494a4
Rokas <rookas.rudzenskas@gmail.com>
2021-10-25 17:41:03 +0300

    Rokas

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -8,20 +8,20 @@ import styles from "./style";
 const HomeScreen = ({ navigation }: RootTabScreenProps<'TabOne'>)  => {
   return (
     <View>
-      <View style={tw`flex-row items-center justify-center p-1 bg-red-500`}>
+      <View style={tw`flex-row items-center justify-center px-3`}>
         <View style={tw`p-3`}>
             <Image source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'}} style={styles.image}/>
         </View>
-          <View style={tw` bg-blue-500 flex-1`}>
+          <View style={tw`flex-1 items-center`}>
               <View style={tw`flex-row items-center`}>
-                  <View style={tw``}>
+                  <View style={tw`flex flex-row flex-1`}>
                     <Text style={tw`text-xl font-bold`}>Rokas Rudzianskas</Text>
                   </View>
                   <View style={tw``}>
                     <Text style={tw`text-lg text-gray-500`}>11:11AM</Text>
                   </View>
               </View>
-              <Text style={tw`text-lg text-gray-500`}>Hola Hola Hola Ola!</Text>
+              <Text style={tw`text-lg text-gray-500 mb-5`}>Hola Hola Hola Ola!</Text>
           </View>
       </View>
     </View>
