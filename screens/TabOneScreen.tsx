f3d29c7d0b538285df4c129bc2e27f24ee7270a2
Rokas <rookas.rudzenskas@gmail.com>
2021-10-25 16:25:12 +0300

    Rokas

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -10,7 +10,6 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   return (
     <View style={styles.container}>
       <Text style={tw`text-xl font-bold`}>Signal 🔥</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
   );
 }
@@ -20,14 +19,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  }
 });
