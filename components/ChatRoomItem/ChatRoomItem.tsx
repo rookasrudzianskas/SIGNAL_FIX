1cf61fae955979b7f00099b0bc9f4420a3b5446f
Rokas <rookas.rudzenskas@gmail.com>
2021-10-25 19:10:37 +0300

    Rokas

--- components/ChatRoomItem/ChatRoomItem.tsx
+++ components/ChatRoomItem/ChatRoomItem.tsx
@@ -11,7 +11,7 @@ const ChatRoomItem = (props) => {
         <TouchableOpacity activeOpacity={0.6}>
             <View style={tw`flex-row px-3`}>
                 <View style={tw`p-3`}>
-                    <Image source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'}} style={styles.image}/>
+                    <Image source={{uri: chatRoom?.imageUri}} style={styles.image}/>
                     <View style={[tw`bg-blue-500 flex items-center justify-center`, {width: 20, height: 20, borderRadius: 20, position: 'absolute', left: 54, top: 10, borderWidth: 1, borderColor: 'white'}]}>
                         <Text style={[tw`text-white  font-bold`, {}]}>4</Text>
                     </View>
@@ -19,13 +19,13 @@ const ChatRoomItem = (props) => {
                 <View style={tw`flex-1 justify-center mt-4`}>
                     <View style={tw`flex-row items-center`}>
                         <View style={tw`flex flex-row flex-1`}>
-                            <Text style={tw`text-xl font-bold`}>Rokas Rudzianskas</Text>
+                            <Text style={tw`text-xl font-bold`}>chatRoom.</Text>
                         </View>
                         <View style={tw``}>
                             <Text style={tw`text-lg text-gray-500`}>11:11AM</Text>
                         </View>
                     </View>
-                    <Text numberOfLines={1} style={tw`text-lg text-gray-500 mb-5`}>Hola Hola Hola Ola!fsdfsafdsfsdfsdfdsfsdfsdhfsdkfsdjkfjsdklf</Text>
+                    <Text numberOfLines={1} style={tw`text-lg text-gray-500 mb-5`}>{chatRoom?.lastMessage?.content}</Text>
                 </View>
             </View>
         </TouchableOpacity>
