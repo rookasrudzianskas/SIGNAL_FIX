0b3957b86aa3fea64f4d476a62fdb71732f73e94
Rokas <rookas.rudzenskas@gmail.com>
2021-10-25 17:45:12 +0300

    Rokas

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -7,24 +7,24 @@ import styles from "./style";
 
 const HomeScreen = ({ navigation }: RootTabScreenProps<'TabOne'>)  => {
   return (
-    <View>
-      <View style={tw`flex-row items-center justify-center px-3`}>
-        <View style={tw`p-3`}>
-            <Image source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'}} style={styles.image}/>
+        <View>
+              <View style={tw`flex-row px-3`}>
+                    <View style={tw`p-3`}>
+                        <Image source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'}} style={styles.image}/>
+                    </View>
+                          <View style={tw`flex-1 justify-center`}>
+                              <View style={tw`flex-row items-center`}>
+                                  <View style={tw`flex flex-row flex-1`}>
+                                        <Text style={tw`text-xl font-bold`}>Rokas Rudzianskas</Text>
+                                  </View>
+                                  <View style={tw``}>
+                                        <Text style={tw`text-lg text-gray-500`}>11:11AM</Text>
+                                  </View>
+                              </View>
+                              <Text style={tw`text-lg text-gray-500 mb-5`}>Hola Hola Hola Ola!</Text>
+                          </View>
+                  </View>
         </View>
-          <View style={tw`flex-1 items-center`}>
-              <View style={tw`flex-row items-center`}>
-                  <View style={tw`flex flex-row flex-1`}>
-                    <Text style={tw`text-xl font-bold`}>Rokas Rudzianskas</Text>
-                  </View>
-                  <View style={tw``}>
-                    <Text style={tw`text-lg text-gray-500`}>11:11AM</Text>
-                  </View>
-              </View>
-              <Text style={tw`text-lg text-gray-500 mb-5`}>Hola Hola Hola Ola!</Text>
-          </View>
-      </View>
-    </View>
   );
 }
 
