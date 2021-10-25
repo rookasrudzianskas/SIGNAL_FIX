2a8bbae12809e62c412ff856075e2419cf4a8ede
Rokas <rookas.rudzenskas@gmail.com>
2021-10-25 20:18:01 +0300

    Rokas

---
+++ screens/ChatRoomScreen/ChatRoomScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const ChatRoomScreen = () => {
+    return (
+        <View>
+            <Text>
+                byrookas 🚀
+            </Text>
+        </View>
+    );
+};
+
+export default ChatRoomScreen;
