d3f707f7415612ffa7f8ab29f01aaaf9de75e3ba
Rokas <rookas.rudzenskas@gmail.com>
2021-10-25 16:51:22 +0300

    Rokas

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -10,10 +10,14 @@ const HomeScreen = ({ navigation }: RootTabScreenProps<'TabOne'>)  => {
     <View>
       <View style={tw`flex-1 flex-row`}>
         <View>
-            <Image source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'}} style={styles.image}/>
+            {/*<Image source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'}} style={styles.image}/>*/}
         </View>
           <View>
-
+              <View>
+                  <Text>Rokas Rudzianskas</Text>
+                  <Text>11:11AM</Text>
+              </View>
+              <Text>Hola Hola Hola Ola!</Text>
           </View>
       </View>
     </View>
