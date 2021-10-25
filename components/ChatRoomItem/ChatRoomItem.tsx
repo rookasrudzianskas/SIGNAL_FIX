3ff4de068913ce17082bf1148bcfe0fc48752732
Rokas <rookas.rudzenskas@gmail.com>
2021-10-25 19:16:17 +0300

    Rokas

--- components/ChatRoomItem/ChatRoomItem.tsx
+++ components/ChatRoomItem/ChatRoomItem.tsx
@@ -4,14 +4,14 @@ import { RootTabScreenProps } from '../../types';
 import tw from "tailwind-react-native-classnames";
 import styles from "./style";
 
-const ChatRoomItem = (props) => {
-    const chatRoom = props.chatRoom;
+const ChatRoomItem = ({chatRoom}) => {
+    const user = chatRoom.users[1];
 
     return (
         <TouchableOpacity activeOpacity={0.6}>
             <View style={tw`flex-row px-3`}>
                 <View style={tw`p-3`}>
-                    <Image source={{uri: chatRoom?.imageUri}} style={styles.image}/>
+                    <Image source={{uri: user?.imageUri}} style={styles.image}/>
                     <View style={[tw`bg-blue-500 flex items-center justify-center`, {width: 20, height: 20, borderRadius: 20, position: 'absolute', left: 54, top: 10, borderWidth: 1, borderColor: 'white'}]}>
                         <Text style={[tw`text-white  font-bold`, {}]}>4</Text>
                     </View>
@@ -19,10 +19,10 @@ const ChatRoomItem = (props) => {
                 <View style={tw`flex-1 justify-center mt-4`}>
                     <View style={tw`flex-row items-center`}>
                         <View style={tw`flex flex-row flex-1`}>
-                            <Text style={tw`text-xl font-bold`}>chatRoom.</Text>
+                            <Text style={tw`text-xl font-bold`}>{user?.name}</Text>
                         </View>
                         <View style={tw``}>
-                            <Text style={tw`text-lg text-gray-500`}>11:11AM</Text>
+                            <Text style={tw`text-lg text-gray-500`}>{chatRoom?.lastMessage?.createdAt}</Text>
                         </View>
                     </View>
                     <Text numberOfLines={1} style={tw`text-lg text-gray-500 mb-5`}>{chatRoom?.lastMessage?.content}</Text>
