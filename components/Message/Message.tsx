cc41fe4b1fe0db3eb2dc50177cf51e16cc4d47b5
Rokas <rookas.rudzenskas@gmail.com>
2021-10-25 20:27:02 +0300

    Rokas

---
+++ components/Message/Message.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const Message = () => {
+    return (
+        <View>
+            <Text>
+                byrookas 🚀
+            </Text>
+        </View>
+    );
+};
+
+export default Message;
