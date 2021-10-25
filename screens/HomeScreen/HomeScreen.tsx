ed9a9afd1d646aa2002da21203177f1cd011e3db
Rokas <rookas.rudzenskas@gmail.com>
2021-10-25 16:30:58 +0300

    Roaks

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 import { RootTabScreenProps } from '../../types';
 import tw from "tailwind-react-native-classnames";
