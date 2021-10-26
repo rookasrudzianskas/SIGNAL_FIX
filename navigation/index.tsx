0c1bab063e7842e782818dcfaa3beaa9c43d1b4e
Rokas <rookas.rudzenskas@gmail.com>
2021-10-26 10:12:36 +0300

    Rokas

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -8,13 +8,14 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { ColorSchemeName, Pressable } from 'react-native';
+import {ColorSchemeName, Image, Pressable, View} from 'react-native';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import HomeScreen from '../screens/HomeScreen/HomeScreen';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 import ChatRoomScreen from "../screens/ChatRoomScreen/ChatRoomScreen";
 import {Text} from 'react-native';
+import {Component} from "react";
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
@@ -49,10 +50,11 @@ function TabBarIcon(props: {
   return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
 }
 
+//@ts-ignore
 const HomeHeader = (props) => {
     return (
-        <Text>
-            Home
-        </Text>
+        <View>
+            <Image source={{uri: 'https://avatars.githubusercontent.com/u/38469892?v=4'}} style={{}} />
+        </View>
     )
 }
