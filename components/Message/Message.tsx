4d87dc549a6ac37704ff46a5811dc92868d6bb80
Rokas <rookas.rudzenskas@gmail.com>
2021-10-26 08:46:23 +0300

    ROkas

--- components/Message/Message.tsx
+++ components/Message/Message.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
-import styles from './style';
 import tw from "tailwind-react-native-classnames";
 
 // @ts-ignore
@@ -24,3 +23,23 @@ const Message = ({message}) => {
 };
 
 export default Message;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#3777f0',
+        padding: 10,
+        margin: 10,
+        borderRadius: 10,
+        maxWidth: '75%',
+    },
+    leftContainerMe: {
+        backgroundColor: ,
+        marginLeft: ,
+        marginRight: ,
+    },
+    rightContainer: {
+        marginRight: ,
+    }
+});
+
+
