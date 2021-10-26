6eb74da75c76dfdc0e0e6dcc32d1c0e87298b104
Rokas <rookas.rudzenskas@gmail.com>
2021-10-26 10:15:27 +0300

    Rokas

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,7 +3,7 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { FontAwesome } from '@expo/vector-icons';
+import {Feather, FontAwesome} from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -53,8 +53,11 @@ function TabBarIcon(props: {
 //@ts-ignore
 const HomeHeader = (props) => {
     return (
-        <View>
-            <Image source={{uri: 'https://avatars.githubusercontent.com/u/38469892?v=4'}} style={{}} />
+        <View style={{flexDirection: 'row', justifyContent: 'space-between',}}>
+            <Image source={{uri: 'https://avatars.githubusercontent.com/u/38469892?v=4'}} style={{width: 30, height: 30, borderRadius: 30}} />
+            <Text style={{}}>Home</Text>
+            <Feather name="camera" size={24} color="black" />
+            <Feather name="edit-2" size={24} color="black" />
         </View>
     )
 }
