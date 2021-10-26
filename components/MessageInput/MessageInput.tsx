4a2a494ca03bdc864da3013d97999e1b7798a0e2
Rokas <rookas.rudzenskas@gmail.com>
2021-10-26 09:09:21 +0300

    Rokas

--- components/MessageInput/MessageInput.tsx
+++ components/MessageInput/MessageInput.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TextInput} from 'react-native';
 import styles from "./style";
 
 const MessageInput = () => {
     return (
         <View style={styles.root}>
             <View style={styles.inputContainer}>
-
+                <TextInput />
             </View>
             <View style={styles.buttonContainer}>
                 <Text>+</Text>
--- components/MessageInput/style.ts
+++ components/MessageInput/style.ts
@@ -2,10 +2,11 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
     root: {
-
+        flexDirection: 'row',
     },
     inputContainer: {
-
+        backgroundColor: 'lightgrey',
+        flex: 1,
     },
     buttonContainer: {
 
