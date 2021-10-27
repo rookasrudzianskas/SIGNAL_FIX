56d1eaf2635a034ec23c5d5d69b9a877bc8c481e
Rokas <rookas.rudzenskas@gmail.com>
2021-10-27 14:40:07 +0300

    Rokas

--- amplify/backend/function/signal2fb03d7aPostConfirmation/src/custom.js
+++ amplify/backend/function/signal2fb03d7aPostConfirmation/src/custom.js
@@ -10,6 +10,19 @@ exports.handler = (event, context, callback) => {
     return;
   }
 
+  const now = new Date();
+  const timestamp = now.getTime();
+
+  const userItem = {
+    __typename: { S: 'User' },
+    _lastChangedAt: { N: timestamp.toString() },
+    _version: { N: "1" },
+    createdAt: { S: now.toISOString() },
+    updatedAt: { S: now.toISOString() },
+    id: { S: event.request.userAttributes.sub },
+    name: { S: event.request.userAttributes.email },
+  }
+
   //save a new user into DynamoDB
 
   callback(null, event);
