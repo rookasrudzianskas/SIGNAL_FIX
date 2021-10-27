44c08a364e656a3040d29027b8e54b4f5805ba4a
Rokas <rookas.rudzenskas@gmail.com>
2021-10-27 14:43:43 +0300

    Rokas

--- amplify/backend/function/signal2fb03d7aPostConfirmation/src/custom.js
+++ amplify/backend/function/signal2fb03d7aPostConfirmation/src/custom.js
@@ -1,6 +1,8 @@
 const aws = require('aws-sdk');
 const ddb = new aws.DynamoDB();
 
+const tableName = process.env.USERTABLE;
+
 exports.handler = (event, context, callback) => {
   // event event.request.userAttributes.sub
   // insert code to be executed by your lambda trigger
