b6ba8dc0e2d1da8a63b749e212958a9acc2f1430
Rokas <rookas.rudzenskas@gmail.com>
2021-10-27 14:35:30 +0300

    Rokas

--- amplify/backend/function/signal2fb03d7aPostConfirmation/src/custom.js
+++ amplify/backend/function/signal2fb03d7aPostConfirmation/src/custom.js
@@ -4,6 +4,12 @@ const ddb = new aws.DynamoDB();
 exports.handler = (event, context, callback) => {
   // event event.request.userAttributes.sub
   // insert code to be executed by your lambda trigger
+
+  if(!event?.request?.userAttributes?.sub) {
+    console.log("No sub provided");
+    return;
+  }
+
   //save a new user into DynamoDB
 
   callback(null, event);
