d2796215ffc81e49774b36bad821f3dbd2a3cd1d
Rokas <rookas.rudzenskas@gmail.com>
2021-10-27 12:48:57 +0300

    Rokas

--- amplify/backend/function/signal2fb03d7aPostConfirmation/src/custom.js
+++ amplify/backend/function/signal2fb03d7aPostConfirmation/src/custom.js
@@ -1,4 +1,5 @@
 exports.handler = (event, context, callback) => {
+  // event event.request.userAttributes.sub
   // insert code to be executed by your lambda trigger
   //save a new user into DynamoDB
 
