d878b28f723f477cde718b71b7dc8e939b4b0115
Rokas <rookas.rudzenskas@gmail.com>
2021-10-27 14:33:38 +0300

    Rokas

--- amplify/backend/function/signal2fb03d7aPostConfirmation/src/custom.js
+++ amplify/backend/function/signal2fb03d7aPostConfirmation/src/custom.js
@@ -1,3 +1,5 @@
+const aws = require('aws-sdk');
+
 exports.handler = (event, context, callback) => {
   // event event.request.userAttributes.sub
   // insert code to be executed by your lambda trigger
