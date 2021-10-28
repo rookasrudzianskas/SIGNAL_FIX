f8a98b8a9fb7d0ea14c2b0bf6eb83c38c7b87798
Rokas <rookas.rudzenskas@gmail.com>
2021-10-28 11:01:53 +0300

    Rokas

---
+++ components/UserItem/UserItem.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import {Image, StyleSheet, TouchableOpacity, View, Text} from 'react-native';
+import { RootTabScreenProps } from '../../types';
+import tw from "tailwind-react-native-classnames";
+import styles from "./style";
+import moment from 'moment';
+import {useNavigation} from "@react-navigation/native";
+
+// @ts-ignore
+const UserItem = ({chatRoom}) => {
+    const user = chatRoom.users[1];
+
+    const navigation = useNavigation();
+    const onPress = () => {
+        // @ts-ignore
+        navigation.navigate('ChatRoomScreen', {
+            id: chatRoom.id,
+        });
+    }
+
+    return (
+        <TouchableOpacity onPress={onPress} activeOpacity={0.6}>
+            <View style={tw`flex-row px-3`}>
+                <View style={tw`p-3`}>
+                    <Image source={{uri: user?.imageUri}} style={styles.image}/>
+                    {chatRoom.newMessages && (
+                        <View style={[tw`bg-blue-500 flex items-center justify-center`, {width: 20, height: 20, borderRadius: 20, position: 'absolute', left: 54, top: 10, borderWidth: 1, borderColor: 'white'}]}>
+                            <Text style={[tw`text-white  font-bold`, {}]}>{chatRoom?.newMessages}</Text>
+                        </View>
+                    )}
+                </View>
+                <View style={tw`flex-1 justify-center mt-4`}>
+                    <View style={tw`flex-row items-center`}>
+                        <View style={tw`flex flex-row flex-1`}>
+                            <Text style={tw`text-xl font-bold`}>{user?.name}</Text>
+                        </View>
+                        <View style={tw``}>
+                            <Text style={tw`text-lg text-gray-500`}>{moment(chatRoom?.lastMessage?.createdAt).fromNow()}</Text>
+                        </View>
+                    </View>
+                    <Text numberOfLines={1} style={tw`text-lg text-gray-500 mb-5`}>{chatRoom?.lastMessage?.content}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+export default UserItem;
---
+++ components/UserItem/style.ts
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    image: {
+        width: 60,
+        height: 60,
+        borderRadius: 50
+    }
+})
+
+export default styles;
