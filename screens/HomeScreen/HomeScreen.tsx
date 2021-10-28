770d139f683e0ec4baeb77381621f02c58b29576
Rokas <rookas.rudzenskas@gmail.com>
2021-10-28 13:40:01 +0300

    Rokas

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -6,15 +6,27 @@ import tw from "tailwind-react-native-classnames";
 import styles from "./style";
 import ChatRoomItem from "../../components/ChatRoomItem";
 import ChatRoomsData from "../../assets/data/ChatRooms";
+import {useEffect, useState} from "react";
+import {ChatRoom} from "../../src/models";
+import {DataStore} from "aws-amplify";
 
 
 const HomeScreen = ({ navigation }: RootTabScreenProps<'TabOne'>)  => {
 
+    const [chatRooms, setChatRooms] = useState<ChatRoom[]>([]);
+
+    useEffect(() => {
+        const fetchChatRooms = async () => {
+            const chatRooms = await DataStore.query(ChatRoom);
+            setChatRooms(chatRooms);
+        };
+        fetchChatRooms();
+    }, []);
 
   return (
         <View style={styles.page}>
 
-            <FlatList showsVerticalScrollIndicator={false} data={ChatRoomsData} renderItem={({item}) => (
+            <FlatList showsVerticalScrollIndicator={false} data={chatRooms} renderItem={({item}) => (
                 <ChatRoomItem key={item.id} chatRoom={item} />
             )}  />
 
