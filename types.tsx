f45a8aa9f5d82110a6e2f0ba2d052bd6632e2f7f
Rokas <rookas.rudzenskas@gmail.com>
2021-10-28 10:47:34 +0300

    Rokas

--- types.tsx
+++ types.tsx
@@ -19,6 +19,7 @@ export type RootStackParamList = {
   NotFound: undefined;
   HomeScreen: undefined;
   ChatRoomScreen: undefined;
+  UsersScreen: undefined;
 };
 
 export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
@@ -31,6 +32,7 @@ export type RootTabParamList = {
   TabTwo: undefined;
   HomeScreen: undefined;
   ChatRoomScreen: undefined;
+  UsersScreen: undefined;
 };
 
 export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
