2f453334fdce8db4cf0c86fe2826f62fb5e7522e
Rokas <rookas.rudzenskas@gmail.com>
2021-10-30 21:08:36 +0300

    Rokas

--- components/ChatRoomHeader/ChatRoomHeader.tsx
+++ components/ChatRoomHeader/ChatRoomHeader.tsx
@@ -5,16 +5,15 @@ import * as React from "react";
 import {useRoute} from "@react-navigation/native";
 
 // @ts-ignore
-const ChatRoomHeader = ({id}) => {
+const ChatRoomHeader = ({id, children}) => {
 
     const {width, height} = useWindowDimensions();
-    console.log(props);
 
     return (
         <View style={{flexDirection: 'row', justifyContent: 'space-between', width: -50, paddingRight: 10, alignItems: 'center'}}>
             {/*<Text>Rokas</Text>*/}
             <Image source={{uri: 'https://avatars.githubusercontent.com/u/38469892?v=4'}} style={{width: 30, height: 30, borderRadius: 30, marginRight: -200}} />
-            <Text style={{flex: 1, textAlign: 'right', marginRight: 70, fontSize: 20, fontWeight: '600',}}>{props?.children}</Text>
+            <Text style={{flex: 1, textAlign: 'right', marginRight: 70, fontSize: 20, fontWeight: '600',}}>{children}</Text>
             <View style={tw`mr-6 flex-row mr-16`}>
                 <Feather style={tw`mr-4`} name="camera" size={24} color="black" />
                 <Feather name="edit-2" size={24} color="black" />
