878729c52a6735a750e1b01adb37d8d1257fb5f1
Rokas <rookas.rudzenskas@gmail.com>
2021-10-30 21:24:00 +0300

    Rokas

--- components/ChatRoomHeader/ChatRoomHeader.tsx
+++ components/ChatRoomHeader/ChatRoomHeader.tsx
@@ -35,16 +35,18 @@ const ChatRoomHeader = ({id, children}) => {
     return (
         <View style={{flexDirection: 'row', justifyContent: 'space-between', width: -50, paddingRight: 10, alignItems: 'center'}}>
             {/*<Text>Rokas</Text>*/}
-            <View>
-                <Image source={{uri: user?.imageUri}} style={{width: 30, height: 30, borderRadius: 30, marginRight: -200}} />
-            </View>
-            <View>
-                <Text style={{flex: 1, fontSize: 20, fontWeight: '600',}}>{user?.name}</Text>
-            </View>
-            <View>
-                <View style={tw`mr-6 flex-row mr-16`}>
-                    <Feather style={tw`mr-4`} name="camera" size={24} color="black" />
-                    <Feather name="edit-2" size={24} color="black" />
+            <View style={tw`flex-row`}>
+                <View style={tw``}>
+                    <Image source={{uri: user?.imageUri}} style={{width: 30, height: 30, borderRadius: 30, marginRight: -200}} />
+                </View>
+                <View style={tw`flex-1 ml-10`}>
+                    <Text style={{ fontSize: 20, fontWeight: '600',}}>{user?.name}</Text>
+                </View>
+                <View style={tw``}>
+                    <View style={tw`mr-6 flex-row mr-16`}>
+                        <Feather style={tw`mr-4`} name="camera" size={24} color="black" />
+                        <Feather name="edit-2" size={24} color="black" />
+                    </View>
                 </View>
             </View>
         </View>
