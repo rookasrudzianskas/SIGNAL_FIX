69cc9b2384c4b5d90e07f8aa3f900c0d150804dc
Rokas <rookas.rudzenskas@gmail.com>
2021-11-08 11:59:37 +0200

    Rokas

--- components/MessageInput/MessageInput.tsx
+++ components/MessageInput/MessageInput.tsx
@@ -153,13 +153,11 @@ const MessageInput = ({chatRoom}) => {
 
     async function startRecording() {
         try {
-            console.log('Requesting permissions..');
-            await Audio.requestPermissionsAsync();
             await Audio.setAudioModeAsync({
                 allowsRecordingIOS: true,
                 playsInSilentModeIOS: true,
             });
-            console.log('Starting recording..');
+            console.log('Recoding is starting');
             const { recording } = await Audio.Recording.createAsync(
                 Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
             );
