d4cbd9b2870b9049d6149dc6c88046bc9f996254
Rokas <rookas.rudzenskas@gmail.com>
2021-11-09 11:29:20 +0200

    Rokas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import Amplify from 'aws-amplify';
+import Amplify, {Hub} from 'aws-amplify';
 import config from './src/aws-exports';
 // @ts-ignore
 import { withAuthenticator } from 'aws-amplify-react-native';
@@ -22,6 +22,20 @@ Amplify.configure({
 const App = () => {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+ useEffect(() => {
+   // Create listener
+   const listener = Hub.listen('datastore', async hubData => {
+     const  { event, data } = hubData.payload;
+     if (event === 'networkStatus') {
+       console.log(`User has a network connection: ${data.active}`)
+     }
+   })
+
+// Remove listener
+   listener();
+ }, []);
+
 
   if (!isLoadingComplete) {
     return null;
