b8827155fb907dd6352cd6783c829613bf5c1807
Rokas <rookas.rudzenskas@gmail.com>
2021-11-15 09:59:23 +0200

    Rokas

--- components/NewGroupButton/NewGroupButton.tsx
+++ components/NewGroupButton/NewGroupButton.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Text, View, StyleSheet, Pressable, TouchableOpacity} from 'react-native';
 import {FontAwesome} from "@expo/vector-icons";
+import tw from "tailwind-react-native-classnames";
 
 const NewGroupButton = () => {
     return (
         <TouchableOpacity activeOpacity={0.5}>
-            <View>
-                <FontAwesome name="group" size={24} color="black" />
-                <Text>New Group</Text>
+            <View style={tw`flex flex-row p-4 items-center`}>
+                <FontAwesome style={tw`flex-1`} name="group" size={24} color="gray" />
+                <Text style={tw`ml-5 font-bold`}>New Group</Text>
             </View>
         </TouchableOpacity>
     );
