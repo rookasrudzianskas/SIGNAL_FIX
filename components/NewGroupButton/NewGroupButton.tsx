21cd27af1ff3e1b8fe79eeb938da42daee276bef
Rokas <rookas.rudzenskas@gmail.com>
2021-11-15 10:50:18 +0200

    Rokas

--- components/NewGroupButton/NewGroupButton.tsx
+++ components/NewGroupButton/NewGroupButton.tsx
@@ -6,14 +6,14 @@ import tw from "tailwind-react-native-classnames";
 // @ts-ignore
 const NewGroupButton = ({onPress}) => {
     return (
-        <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
+        <View >
             <View style={tw`flex flex-row p-4 items-center`}>
                 <FontAwesome style={tw`flex-1`} name="group" size={24} color="#4f4f4f" />
-                <TouchableOpacity activeOpacity={0.5}>
+                <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
                     <Text style={tw`ml-5 font-bold`}>New Group</Text>
                 </TouchableOpacity>
             </View>
-        </TouchableOpacity>
+        </View>
     );
 };
 
