c6b7dd83b9e939c82d0dee8295a5e38c57761eaf
Rokas <rookas.rudzenskas@gmail.com>
2021-11-17 11:45:34 +0200

    Rokas

--- screens/GroupInfoScreen/GroupInfoScreen.tsx
+++ screens/GroupInfoScreen/GroupInfoScreen.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {ChatRoom} from "../../src/models";
 
 const GroupInfoScreen = () => {
+
+    const [chatRoom, setChatRoom] = useState<ChatRoom | null>(null);
+
     return (
         <View>
             <Text>
