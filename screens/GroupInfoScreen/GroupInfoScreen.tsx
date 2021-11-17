ee1bc5da24fcb0a2fff47606be597be1696c8af6
Rokas <rookas.rudzenskas@gmail.com>
2021-11-17 11:36:36 +0200

    Roaks

---
+++ screens/GroupInfoScreen/GroupInfoScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const GroupInfoScreen = () => {
+    return (
+        <View>
+            <Text>
+                byrookas 🚀
+            </Text>
+        </View>
+    );
+};
+
+export default GroupInfoScreen;
