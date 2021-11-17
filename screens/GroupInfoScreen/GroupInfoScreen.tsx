20405f5ad5358e7418fa2a8a70faa1cc45ddbf56
Rokas <rookas.rudzenskas@gmail.com>
2021-11-17 12:16:40 +0200

    Rokas

--- screens/GroupInfoScreen/GroupInfoScreen.tsx
+++ screens/GroupInfoScreen/GroupInfoScreen.tsx
@@ -1,21 +1,32 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import {ChatRoom, User} from "../../src/models";
-import {DataStore} from "aws-amplify";
+import {Text, View, StyleSheet, FlatList} from 'react-native';
+import {ChatRoom, ChatRoomUser, User} from "../../src/models";
+import {Auth, DataStore} from "aws-amplify";
 import {useRoute} from "@react-navigation/native";
 import tw from "tailwind-react-native-classnames";
+import UserItem from "../../components/UserItem";
 
 const GroupInfoScreen = () => {
 
     const [chatRoom, setChatRoom] = useState<ChatRoom | null>(null);
     const [allUsers, setAllUsers] = useState<User[]>([]);
 
+
     const route = useRoute();
     // console.log(chatRoom);
 
     useEffect(() => {
         fetchChatRoom();
+        fetchUsers();
     }, []);
+
+    const fetchUsers = async () => {
+        const fetchedUsers = (await DataStore.query(ChatRoomUser))
+            .filter(chatRoomUser => chatRoomUser.chatroom.id === id)
+            .map(chatRoomUser => chatRoomUser.user);
+
+        setAllUsers(fetchedUsers);
+    };
 
     const fetchChatRoom = async () => {
         // @ts-ignore
@@ -41,6 +52,8 @@ const GroupInfoScreen = () => {
             </View>
             <View style={tw``}>
                 <Text style={tw`text-xl font-bold`}>Users</Text>
+
+                <FlatList data={allUsers} renderItem={({ item }) => <UserItem user={item} />} />
             </View>
 
         </View>
