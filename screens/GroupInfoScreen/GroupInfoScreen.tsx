89c6f0512e6237a2ac1bb6c89a271ebf4f12b81d
Rokas <rookas.rudzenskas@gmail.com>
2021-11-17 11:47:01 +0200

    Roaks

--- screens/GroupInfoScreen/GroupInfoScreen.tsx
+++ screens/GroupInfoScreen/GroupInfoScreen.tsx
@@ -1,10 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {ChatRoom} from "../../src/models";
+import {DataStore} from "aws-amplify";
 
 const GroupInfoScreen = () => {
 
     const [chatRoom, setChatRoom] = useState<ChatRoom | null>(null);
+
+    useEffect(() => {
+        fetchChatRoom();
+    }, []);
+
+    const fetchChatRoom = async () => {
+        // @ts-ignore
+        if(!route?.params?.id) {
+            console.warn('No chat id is provided.');
+            return;
+        }
+        // @ts-ignore
+        const chatRoom = await DataStore.query(ChatRoom, route?.params?.id);
+
+        if(!chatRoom) {
+            console.error("Coud not find the chat room");
+        } else {
+            setChatRoom(chatRoom);
+        }
+        // const fetchMessages = await DataStore.query(MessageModel);
+    };
 
     return (
         <View>
