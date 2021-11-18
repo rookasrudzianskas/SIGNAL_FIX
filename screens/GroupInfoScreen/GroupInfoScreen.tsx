65a953e548e5726054c78acb8e2cc980a2bc864f
Rokas <rookas.rudzenskas@gmail.com>
2021-11-18 09:51:11 +0200

    Rokas

--- screens/GroupInfoScreen/GroupInfoScreen.tsx
+++ screens/GroupInfoScreen/GroupInfoScreen.tsx
@@ -47,6 +47,7 @@ const GroupInfoScreen = () => {
     };
 
     const confirmDelete = (user: any) => {
+        // @ts-ignore
         if(user.id === chatRoom?.Admin.id) {
             Alert.alert('Cannot delete the group admin');
             return;
