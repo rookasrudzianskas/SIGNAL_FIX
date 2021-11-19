dc277624b1ef8deccc30d3d7fbb17d65e687c473
Rokas <rookas.rudzenskas@gmail.com>
2021-11-19 10:09:52 +0200

    Rokas

--- screens/ChatRoomScreen/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen/ChatRoomScreen.tsx
@@ -72,12 +72,16 @@ const ChatRoomScreen = () => {
         return <ActivityIndicator size={'large' } color={'green'} />
     }
 
+    const deleteConfirmation = (message: MessageModel) => {
+
+    }
+
     // console.log(messageReplyTo?.content);
 
     return (
         <SafeAreaView style={styles.container}>
             <FlatList  showsVerticalScrollIndicator={false}  inverted data={messages} renderItem={({item}) => (
-                <Message message={item} setAsMessageReply={() => setMessageReplyTo(item)} />
+                <Message message={item} setAsMessageReply={() => setMessageReplyTo(item)} onLongPress={() => deleteConfirmation(item)} />
             )} />
             <MessageInput chatRoom={chatRoom}  messageReplyTo={messageReplyTo} removeMessageReplyTo={() => setMessageReplyTo(null)} />
         </SafeAreaView>
