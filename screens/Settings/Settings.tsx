6d33dfb3d25a0303022ec090deff9303b3f4c7b3
Rokas <rookas.rudzenskas@gmail.com>
2021-11-20 20:58:35 +0200

    Rokas

--- screens/Settings/Settings.tsx
+++ screens/Settings/Settings.tsx
@@ -14,7 +14,7 @@ const SettingsScreen = () => {
     return (
         <View style={tw`p-4`}>
                 <TouchableOpacity onPress={logOut} activeOpacity={0.5} style={tw`flex bg-blue-600 px-6 py-3 items-center justify-center m-4 rounded-lg`}>
-                    <Text style={tw`text-gray-100 font-bold`}>Log out</Text>
+                    <Text style={tw`text-gray-100 font-bold`}>Update Key Pair</Text>
                 </TouchableOpacity>
 
                 <TouchableOpacity activeOpacity={0.5} style={tw` flex bg-blue-600 px-6 py-3 items-center justify-center mx-4 rounded-lg`}>
