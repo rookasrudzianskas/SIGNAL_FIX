fe5430b313029f85c2c500ba4889feee8dfd5d0a
Rokas <rookas.rudzenskas@gmail.com>
2021-11-20 19:54:00 +0200

    Rokas

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,4 +1,11 @@
 import {getRandomBytes} from "expo-random";
+import { box, randomBytes } from 'tweetnacl';
+import {
+        decodeUTF8,
+        encodeUTF8,
+        encodeBase64,
+        decodeBase64
+} from 'tweetnacl-util';
 
 export const PRNG = (x, n) => {
         // we get random bytes
@@ -8,4 +15,44 @@ export const PRNG = (x, n) => {
         }
 };
 
+export const encrypt = (
+        secretOrSharedKey,
+        json,
+        key) => {
+        const nonce = newNonce();
+        const messageUint8 = decodeUTF8(JSON.stringify(json));
+        const encrypted = key
+                ? box(messageUint8, nonce, key, secretOrSharedKey)
+                : box.after(messageUint8, nonce, secretOrSharedKey);
+
+        const fullMessage = new Uint8Array(nonce.length + encrypted.length);
+        fullMessage.set(nonce);
+        fullMessage.set(encrypted, nonce.length);
+
+        const base64FullMessage = encodeBase64(fullMessage);
+        return base64FullMessage;
+};
+
+export const decrypt = (
+        secretOrSharedKey,
+        messageWithNonce,
+        key) => {
+        const messageWithNonceAsUint8Array = decodeBase64(messageWithNonce);
+        const nonce = messageWithNonceAsUint8Array.slice(0, box.nonceLength);
+        const message = messageWithNonceAsUint8Array.slice(
+                box.nonceLength,
+                messageWithNonce.length
+        );
+
+        const decrypted = key
+                ? box.open(message, nonce, key, secretOrSharedKey)
+                : box.open.after(message, nonce, secretOrSharedKey);
+
+        if (!decrypted) {
+                throw new Error('Could not decrypt message');
+        }
+
+        const base64DecryptedMessage = encodeUTF8(decrypted);
+        return JSON.parse(base64DecryptedMessage);
+};
 
