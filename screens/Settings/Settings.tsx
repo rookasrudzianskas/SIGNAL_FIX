88d18bd965f480120d8a8340aa0d877594014392
Rokas <rookas.rudzenskas@gmail.com>
2021-11-20 20:46:14 +0200

    Rokas

--- screens/Settings/Settings.tsx
+++ screens/Settings/Settings.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import tw from "tailwind-react-native-classnames";
+import {Auth, DataStore} from "aws-amplify";
 
 const SettingsScreen = () => {
+
+    const logOut = async () => {
+        await DataStore.clear();
+        await Auth.signOut();
+    }
+
+
     return (
         <View>
             <View style={tw`bg-blue-600 px-6 py-3 items-center justify-center`}>
