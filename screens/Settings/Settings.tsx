c5f6c706ff031226d75d16f5ad216aa0a9db5939
Rokas <rookas.rudzenskas@gmail.com>
2021-11-20 20:57:54 +0200

    Rokas

--- screens/Settings/Settings.tsx
+++ screens/Settings/Settings.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 import tw from "tailwind-react-native-classnames";
 import {Auth, DataStore} from "aws-amplify";
 
@@ -13,13 +13,13 @@ const SettingsScreen = () => {
 
     return (
         <View style={tw`p-4`}>
-                <View style={tw`flex bg-blue-600 px-6 py-3 items-center justify-center m-4 rounded-lg`}>
+                <TouchableOpacity onPress={logOut} activeOpacity={0.5} style={tw`flex bg-blue-600 px-6 py-3 items-center justify-center m-4 rounded-lg`}>
                     <Text style={tw`text-gray-100 font-bold`}>Log out</Text>
-                </View>
+                </TouchableOpacity>
 
-                <View style={tw` flex bg-blue-600 px-6 py-3 items-center justify-center mx-4 rounded-lg`}>
+                <TouchableOpacity activeOpacity={0.5} style={tw` flex bg-blue-600 px-6 py-3 items-center justify-center mx-4 rounded-lg`}>
                     <Text style={tw`text-gray-100 font-bold`}>Settings</Text>
-                </View>
+                </TouchableOpacity>
         </View>
     );
 };
